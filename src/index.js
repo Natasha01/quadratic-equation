fc54131333ae96f04dc154fcde39a7475a9e63af
Natasha01 <borodycknatasha01@mail.ru>
2018-02-25 13:14:58 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+
+  var strArray = equation.split('x'); // разбивает на куски
+
+  var str = strArray[0];
+  var a = parseInt(str, 10);
+  
+  str = strArray[1];
+  str = str[3] + str.substr(5);
+  var b = parseInt(str, 10);
+
+  str = strArray[2];
+  str = str[1] + str.substr(3);
+  var c = parseInt(str, 10);
+  
+  var D = b*b - 4*a*c;
+  var res_arr = [];
+  var x = (-b + Math.sqrt(D)) / (2*a);
+  res_arr.push(Math.round(x));
+  x = (-b - Math.sqrt(D)) / (2*a);
+  if (x > res_arr[0]){
+    res_arr.push(Math.round(x));
+  } else{
+    res_arr.unshift(Math.round(x));
+  }
+  
+   
+  return res_arr;
 }
